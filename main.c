bc239f7163dd555c3a8f0523f990e826a927d9d0
OHMG <oscar_mg2893@hotmail.com>
2014-10-24 11:02:20 -0500

    Implementacion de la funcion Mod
    Realiza mod entre 2 numeros ingresados pro teclado

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+void mod_funcion(int a, int b);
 int main() {
 
     int a,b; 
@@ -10,10 +10,13 @@ int main() {
 	scanf ("%d",&a); 
     printf ("Segundo numero: ");  
 	scanf ("%d",&b); 
-	
+	mod_funcion(a,b);
     //implementar resta, multiplicacion, division
    
    
     return (EXIT_SUCCESS);
 }
 
+void mod_funcion(int a, int b){
+	printf("El mod de %d con %d es: %d",a,b,a%b);
+}

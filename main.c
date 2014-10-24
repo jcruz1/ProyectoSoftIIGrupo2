93bd22ff8983083aab92c22975a72a4c1e396f5c
Joe <joe.endor@hotmail.com>
2014-10-24 10:52:41 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sumar(int a, int b);
+
+int main() {
+
+    int a,b; 
+	printf ("Bienvenido... Ingrese 2 numeros!");
+    printf ("Primer numero: ");   
+	scanf ("%d",&a); 
+    printf ("Segundo numero: ");  
+	scanf ("%d",&b); 
+	
+    //implementar resta, multiplicacion, division
+   
+    return (EXIT_SUCCESS);
+}
+
